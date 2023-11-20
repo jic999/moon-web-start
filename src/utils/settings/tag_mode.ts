0a42d175ba494b115e32e86e8de6a00f3635533d
jic999 <jic999@qq.com>
2023-11-21 06:30:47 +0800

    fix: setting types

--- src/utils/settings/tag_mode.ts
+++ src/utils/settings/tag_mode.ts
@@ -2,8 +2,8 @@ import type { SettingItemChildren, TagMode } from '@/types'
 import { SettingItem } from '@/types'
 
 const tagModeList: SettingItemChildren<TagMode> = [
-  { name: () => t('settings.tagMode.concise'), key: 'Concise' },
-  { name: () => t('settings.tagMode.full'), key: 'Full' },
+  { name: () => t('settings.tagMode.concise'), key: 'Concise', value: 'Concise' },
+  { name: () => t('settings.tagMode.full'), key: 'Full', value: 'Full' },
 ]
 
 export const tagMode = new SettingItem({
--- src/utils/settings/website_preference.ts
+++ src/utils/settings/website_preference.ts
@@ -2,10 +2,10 @@ import type { SettingItemChildren, WebsitePreference } from '@/types'
 import { SettingItem } from '@/types'
 
 const websitePreferenceList: SettingItemChildren<WebsitePreference> = [
-  { name: () => t('settings.sitePreference.auto'), key: 'Auto' },
-  { name: () => t('settings.sitePreference.chineseMainland'), key: 'ChineseMainland' },
-  { name: () => t('settings.sitePreference.global'), key: 'Global' },
-  { name: () => t('settings.sitePreference.customize'), key: 'Customize' },
+  { name: () => t('settings.sitePreference.auto'), key: 'Auto', value: 'Auto' },
+  { name: () => t('settings.sitePreference.chineseMainland'), key: 'ChineseMainland', value: 'ChineseMainland' },
+  { name: () => t('settings.sitePreference.global'), key: 'Global', value: 'Global' },
+  { name: () => t('settings.sitePreference.customize'), key: 'Customize', value: 'Customize' },
 ]
 
 export const websitePreference = new SettingItem({
